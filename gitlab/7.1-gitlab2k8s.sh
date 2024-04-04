d2e8f7c2a465f4dad5beab4fb284bf05684a9a68
Mike <mickpav@gmail.com>
2024-04-05 08:17:01 +1000

    project

--- gitlab/7.1-gitlab2k8s.sh
+++ gitlab/7.1-gitlab2k8s.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-helm install gitlab . -f values.yaml -n gl
+helm install gitlab . -f values.yaml -n gitlab
 kubectl get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 -d ; echo
